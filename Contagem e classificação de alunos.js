5e74f1086d3fd995f75652df908ae3a25fe1575b
xWills <98777468+xWills@users.noreply.github.com>
2022-02-25 14:25:02 -0300

    Adicionando resolução da atividade remota 2
    Codificação para contagem de alunos e definição se número é par ou ímpar.

---
+++ Contagem e classificação de alunos.js
@@ -0,0 +1,17 @@
+let QuantidadeAlunos = 10;
+
+for (let i=0 ; i<=QuantidadeAlunos; i++) {
+    if (i == 0) {
+
+        console.log(`${i} é zero`);
+
+    } else if (i%2 == 0){
+
+                console.log(`${i} é par`);
+
+        } else {
+
+            console.log(`${i} é impar`);
+            
+        }
+    }
